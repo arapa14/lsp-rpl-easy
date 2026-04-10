ed946622dcbfd536898fef365cc651cd40edb251
arapa14 <arapa00016@gmail.com>
2026-04-11 00:18:06 +0700

    SETUP ENV, DATABASE, MIGRATION, MODEL DAN SEEDER

---
+++ app/Models/Booking.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Booking extends Model
+{
+    protected $table = 'bookings';
+
+    protected $fillable = [
+        'user_id',
+        'schedule_id',
+        'total_seats',
+        'total_price',
+    ];
+
+    public function schedule()
+    {
+        return $this->belongsTo(Schedule::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    } 
+}
